eb3f3e4fa9a623b31563030bb2ce15449d915ed7
Declan Escott <declanescott@gmail.com>
2023-06-06 20:46:49 +1000

    exit on SDL_Renderer create fail

--- src/app.c
+++ src/app.c
@@ -17,6 +17,7 @@ App* create_app(const char* name, int width, int height) {
 	app->rend = SDL_CreateRenderer(app->win, -1, 0);
 	if (app->rend == NULL) {
 		fprintf(stderr, "Could not create SDL rednerer, SDL error: %s\n", SDL_GetError());
+		exit(1);
 	}
 
 	return app;
