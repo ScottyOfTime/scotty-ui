b00bd6ddc67c5a50e19dc328115377a14f59e87a
Declan Escott <declanescott@gmail.com>
2023-06-06 21:13:06 +1000

    able to quit demo

--- demo.c
+++ demo.c
@@ -7,8 +7,15 @@ int main(int argc, char** argv) {
 	printf("demo\n");
 	App* app = create_app("demo", 640, 480);
 	create_screen(app);
-	while (1) {
+	uint8_t quit = 0;
+	while (!quit) {
 		update_app(app);
+		SDL_Event e;
+		while(SDL_PollEvent(&e) != 0) {
+			if (e.type == SDL_QUIT) {
+				quit = 1;
+			}
+		}
 	}
 	destroy_app(app);
 	return 0;
